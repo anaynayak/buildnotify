97ceaf6e0e692ad94d5c8d4e80efc6a26f8313e3
Anay Nayak <anayak007@gmail.com>
2010-02-13 18:06:50 +0530

    deleting unnecessary files

--- delete.sh
+++
@@ -1,6 +0,0 @@
-sudo rm -Rf /usr/local/share/buildnotify
-sudo rm -Rf /usr/local/lib/python2.6/dist-packages/buildnotifylib/
-sudo rm -Rf /usr/local/bin/buildnotifyapplet.py
-sudo rm -Rf /usr/lib/bonobo/servers/GNOME_BuildNotifyApplet.server
-sudo rm -Rf /usr/local/lib/python2.6/dist-packages/BuildNotify-0.01.egg-info
-
