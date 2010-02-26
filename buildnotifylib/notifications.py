1efc297f47d16299743666572fbffc0ed9df70b1
Anay Nayak <anayak007@gmail.com>
2010-02-26 13:37:05 +0530

    Fixed missing pynotify library issue for windows/mac users

--- buildnotifylib/notifications.py
+++ buildnotifylib/notifications.py
@@ -8,12 +8,16 @@ class Notification:
     def __init__(self, widget):
         self.widget = widget
         self.notification = None
-        if pynotify.init(" buildnotify "):
-            self.notification = pynotify.Notification("buildnotify", "buildnotify", None, None)
+        try:
+            if pynotify.init(" buildnotify "):
+                self.notification = pynotify.Notification("buildnotify", "buildnotify", None, None)
+        except NameError:
+            pass
 
     def show_message(self, title, text):
         if self.notification:
             self.notification.update(title, text, None)
         if self.notification == None or not self.notification.show():
             self.widget.showMessage(title, text, QtGui.QSystemTrayIcon.Information, 3000)
+
 
