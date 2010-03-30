f160cdf6c8e788e47ad9e1faf96969d3ea71f622
Anay Nayak <anayak007@gmail.com>
2010-03-30 10:22:14 +0530

    Removed support for docs, icons and app shortcut as it causes issues with easy_install. Would be reintroduced later once issues are fixed

--- setup.py
+++ setup.py
@@ -1,39 +1,9 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import sys, os
-
-packages_path = os.path.abspath(os.path.dirname(__file__))
-
-datafiles = []
-icon_files = []
-doc_files = []
-
-prefix = sys.prefix + '/'
-ipath_desktop_file = '%sshare/applications/' % prefix
-ipath_icons = '%sshare/pixmaps/' % prefix
-path_images = packages_path + '/icons/'
-ipath_docs = '%sshare/doc/buildnotify/' % prefix
-
-def append_if_valid(target, tuple):
-    if os.path.exists(tuple[0]):
-        target.append(tuple)
-        
-append_if_valid(datafiles, (ipath_desktop_file,[packages_path + '/buildnotify.desktop']))
-
-docs = ['AUTHORS','INSTALL','LICENSE','README','THANKS']
-for doc in docs:
-	doc_files.append(packages_path + '/' + doc)
-
-icons = ['buildnotify.png']
-for icon in icons:
-    icon_files.append(path_images + icon)
-
-append_if_valid(datafiles, (ipath_icons, icon_files))
-append_if_valid(datafiles, (ipath_docs, doc_files))
 
 setup (name='BuildNotify',
-       version='0.2.4',
+       version='0.2.5',
        description='Cruise Control build monitor for Windows/Linux/Mac',
        keywords='cctray ccmenu buildnotify ubuntu linux cruisecontrol continuous integration ci',
        author='Anay Nayak',
@@ -45,6 +15,5 @@ setup (name='BuildNotify',
        'It was largely inspired from CCMenu and lets you monitor multiple continuous integration servers with' +
        'customizable build notifications for all projects',
        packages=['buildnotifylib'],
-       data_files = datafiles,
        scripts = ['buildnotifyapplet.py'])
 
