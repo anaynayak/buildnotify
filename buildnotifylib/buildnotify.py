a7a8114545a7e66bd7cdb65e6bb64339c607bc71
Anay Nayak <anayak007@gmail.com>
2010-05-27 22:35:03 -0500

    Added support to generate deb, buildnotify is now loaded after some delay to avoid problems on faster machines

--- buildnotifylib/buildnotify.py
+++ buildnotifylib/buildnotify.py
@@ -2,47 +2,49 @@ from app_ui import AppUi
 from app_notification import AppNotification
 from config import Config
 from projects import ProjectsPopulator
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 import sys
 import build_icons
+from timed_event import TimedEvent, RepeatTimedEvent
 
 class BuildNotify:
     def __init__(self):
         self.conf = Config()
-        app = QtGui.QApplication(sys.argv)
-        buildIcons = build_icons.BuildIcons()
-        icon = buildIcons.for_status("Success.Sleeping")
-        app.setWindowIcon(icon)
-        if not QtGui.QSystemTrayIcon.isSystemTrayAvailable():
-            QtGui.QMessageBox.critical(None, "BuildNotify",
-                    "I couldn't detect any system tray on this system.")
-            sys.exit(1)
-
+        self.app = QtGui.QApplication(sys.argv)
+        self.buildIcons = build_icons.BuildIcons()
+        icon = self.buildIcons.for_status("Success.Sleeping")
+        self.app.setWindowIcon(icon)
+        self.timed_event = RepeatTimedEvent(self.app, self.delayed_start, 5)
         QtGui.QApplication.setQuitOnLastWindowClosed(False)
-        self.run_app(app, buildIcons)
-        sys.exit(app.exec_())
+        self.timed_event.start()
+        sys.exit(self.app.exec_())
 
-    def run_app(self, app, buildIcons):
+    def delayed_start(self, event_count):
+        if not QtGui.QSystemTrayIcon.isSystemTrayAvailable():
+            if (event_count == 5) :
+                QtGui.QMessageBox.critical(None, "BuildNotify",
+                    "I couldn't detect any system tray on this system.")
+                sys.exit(1)
+            self.timed_event.start()
+            
+        self.run_app()
+
+    def run_app(self):
         self.projects_populator = ProjectsPopulator(self.conf)
-        self.app_ui = AppUi(self.conf, buildIcons)
+        self.app_ui = AppUi(self.conf, self.buildIcons)
         self.app_notification = AppNotification(self.conf, self.app_ui.tray)
         self.projects_populator.add_listener(self.app_notification)
         self.projects_populator.add_listener(self.app_ui)
-        self.auto_poll(app)
-        self.check_nodes()
+        self.auto_poll()
 
-    def auto_poll(self, app):
-        self.timer = QtCore.QTimer()
-        app.connect(self.timer, QtCore.SIGNAL('timeout()'), self.check_nodes)
-        self.timer.setInterval(1000)
-        self.timer.setSingleShot(True)
-        self.timer.start()
+    def auto_poll(self):
+        self.timed_event = TimedEvent(self.app, self.check_nodes)
+        self.check_nodes();
 
     def check_nodes(self):
         self.projects_populator.load_from_server(self.conf)
-        self.timer.setInterval(self.conf.check_interval * 1000)
-        self.timer.setSingleShot(True)
-        self.timer.start()
+        self.timed_event.set_interval(self.conf.check_interval * 1000)
+        self.timed_event.start()
 
 if __name__== '__main__':
     BuildNotify()
---
+++ buildnotifylib/timed_event.py
@@ -0,0 +1,35 @@
+from PyQt4 import QtCore
+
+class TimedEvent:
+    def __init__(self,parent, event_target):
+        self.event_target = event_target
+        self.parent = parent
+        self.interval = 2000
+        
+    def start(self):
+        self.timer = QtCore.QTimer()
+        self.parent.connect(self.timer, QtCore.SIGNAL('timeout()'), self.event_target)
+        print self.interval
+        self.timer.setInterval(self.interval)
+        self.timer.setSingleShot(True)
+        self.timer.start()
+
+    def set_interval(self, interval):
+        self.interval = interval
+
+class RepeatTimedEvent:
+    def __init__(self, parent, event_target, repeat_count):
+        self.parent = parent
+        self.repeat_count = repeat_count
+        self.event_target = event_target
+        self.event_happened_count = 0
+
+    def start(self):
+        self.timed_event = TimedEvent(self.parent, self.on_event)
+        self.timed_event.start()
+
+    def on_event(self):
+        self.event_target(self.event_happened_count);
+        self.event_happened_count = self.event_happened_count + 1
+        if (self.event_happened_count != self.repeat_count):
+            self.start()
