dcddb63f273cf897ad094770b5ea46de9bd1a40e
Anay Nayak <anayak007@gmail.com>
2010-09-01 00:29:42 +0530

    Restored setup.py so that the stdeb methods can be used for generating the deb.Added target to paver to automate deployment.

--- pavement.py
+++ pavement.py
@@ -1,24 +1,8 @@
 from paver.easy import *
-from paver.setuputils import setup
-
-setup (name='BuildNotify',
-       version='0.2.7',
-       description='Cruise Control build monitor for Windows/Linux/Mac',
-       keywords='cctray ccmenu buildnotify ubuntu linux cruisecontrol continuous integration ci',
-       author='Anay Nayak',
-       requires = ['pytz'],
-       author_email='anayak007@gmail.com',
-       url = "http://bitbucket.org/Anay/buildnotify/",
-       license='GPL v3',
-       long_description = 'BuildNotify is a cruise control system tray monitor which works on Windows/Linux/Mac.' +
-       'It was largely inspired from CCMenu and lets you monitor multiple continuous integration servers with' +
-       'customizable build notifications for all projects',
-       packages=['buildnotifylib'],
-       scripts = ['buildnotifyapplet.py'])
 
 @task
 def cleanup():
-    for fl in ['BuildNotify.egg-info', 'build', 'dist']:
+    for fl in ['BuildNotify.egg-info', 'build', 'dist', 'deb_dist']:
         p = path(fl)
         p.rmtree()
         
@@ -26,3 +10,15 @@ def cleanup():
 def mk_resources():
     sh('pyuic4 -o buildnotifylib/preferences_ui.py data/preferences.ui')
     sh('pyuic4 -o buildnotifylib/server_configuration_ui.py data/server_configuration.ui')
+
+@task
+@needs('cleanup')
+def dist():
+    sh('python setup.py --command-packages=stdeb.command sdist_dsc')
+    dist_package = path('deb_dist').dirs('buildnotify-*')[0]
+    sh('sed -i s/unstable/lucid/ %s/debian/changelog' % dist_package)
+    sh('cd %s;dpkg-buildpackage -i -I -rfakeroot' % dist_package)
+    changes_file = path('deb_dist').files('*.changes')[0]
+    sh('dput ppa:anay/ppa %s' % changes_file)
+
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup (name='BuildNotify',
+       version='0.2.7',
+       description='Cruise Control build monitor for Windows/Linux/Mac',
+       keywords='cctray ccmenu buildnotify ubuntu linux cruisecontrol continuous integration ci',
+       author='Anay Nayak',
+       requires = ['pytz'],
+       author_email='anayak007@gmail.com',
+       url = "http://bitbucket.org/Anay/buildnotify/",
+       license='GPL v3',
+       long_description = 'BuildNotify is a cruise control system tray monitor which works on Windows/Linux/Mac.' +
+       'It was largely inspired from CCMenu and lets you monitor multiple continuous integration servers with' +
+       'customizable build notifications for all projects',
+       packages=['buildnotifylib'],
+       scripts = ['buildnotifyapplet.py'])
+
