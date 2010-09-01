9de7aadea80e16e299b136fb3eb37f13279e005a
Anay Nayak <anayak007@gmail.com>
2010-09-01 10:16:14 +0530

    Added tasks to upload to ppa and pypi

--- pavement.py
+++ pavement.py
@@ -1,7 +1,7 @@
 from paver.easy import *
 
 @task
-def cleanup():
+def clean():
     for fl in ['BuildNotify.egg-info', 'build', 'dist', 'deb_dist']:
         p = path(fl)
         p.rmtree()
@@ -12,13 +12,23 @@ def mk_resources():
     sh('pyuic4 -o buildnotifylib/server_configuration_ui.py data/server_configuration.ui')
 
 @task
-@needs('cleanup')
+@needs('dist_pypi', 'dist_ppa')
 def dist():
+    pass
+
+@task
+@needs('clean')
+def dist_pypi():
+    sh('python setup.py sdist upload')
+
+    
+@task
+@needs('clean')
+def dist_ppa():
     sh('python setup.py --command-packages=stdeb.command sdist_dsc')
     dist_package = path('deb_dist').dirs('buildnotify-*')[0]
     sh('sed -i s/unstable/lucid/ %s/debian/changelog' % dist_package)
-    sh('cd %s;dpkg-buildpackage -i -I -rfakeroot' % dist_package)
+    sh('cd %s;dpkg-buildpackage -i -S -I -rfakeroot' % dist_package)
     changes_file = path('deb_dist').files('*.changes')[0]
     sh('dput ppa:anay/ppa %s' % changes_file)
-
-
+    
