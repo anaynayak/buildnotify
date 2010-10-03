92b88732eee5c82e453cdc4b54b0f259b545a376
Anay Nayak <anayak007@gmail.com>
2010-10-03 16:14:29 +0530

    Added task to push changes to opensuse buildservice

--- pavement.py
+++ pavement.py
@@ -36,3 +36,19 @@ def dist_ppa():
 @needs('clean')
 def mk_deb():
     sh('python setup.py --command-packages=stdeb.command bdist_deb')
+
+@task
+@needs('clean')
+def mk_osc():
+    sh('python setup.py sdist')
+    sh('python setup.py --command-packages=stdeb.command sdist_dsc --force-buildsystem=False')
+    dist_package = path('deb_dist').dirs('buildnotify-*')[0]
+    sh('rm %s' % path('../osc_buildnotify').files('*.tar.gz')[0])
+    sh('rm %s' % path('../osc_buildnotify').files('*.dsc')[0])
+    sh('cp %s/debian/changelog ../osc_buildnotify/debian.changelog' % dist_package)
+    sh('cp %s/debian/control ../osc_buildnotify/debian.control' % dist_package)
+    sh('cp %s/debian/rules ../osc_buildnotify/debian.rules' % dist_package)
+    sh('cp %s ../osc_buildnotify/' % path('dist').files('BuildNotify-*')[0])
+    sh('cp %s ../osc_buildnotify/' % path('deb_dist').files('buildnotify*.dsc')[0])
+    sh('osc addremove ../osc_buildnotify/')
+    sh('osc commit ../osc_buildnotify/ -m"Updated package"')
