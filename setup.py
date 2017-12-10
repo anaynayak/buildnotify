9cde2996de0f3d5b2d918ea85d87d0ee564acfe1
Anay Nayak <anayak007@gmail.com>
2017-12-10 12:49:49 +0530

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='BuildNotify',
-      version="1.0.0",
+      version="1.0.1",
       description='Cruise Control build monitor for Windows/Linux/Mac',
       keywords='cctray ccmenu buildnotify ubuntu linux cruisecontrol continuous integration ci',
       author='Anay Nayak',
